d890722e3a07adf1dacbafc0de6bdaec2afc345d
Ihor K <lov3catch@gmail.com>
2021-07-15 10:33:40 +0300

    - init project

---
+++ project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b263b572c8ca1d02bb9a656251fff01f'),
+    },
+  },
+});
