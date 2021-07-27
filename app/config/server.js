0bc2b24dfd1553e0089b3376c3dfd6625002ba3c
Ihor K <lov3catch@gmail.com>
2021-07-27 21:24:44 +0300

    - rollback

--- app/config/server.js
+++ app/config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 8080),
-  url: 'http://localhost',
+  url: 'http://hxlt.com',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b263b572c8ca1d02bb9a656251fff01f'),
