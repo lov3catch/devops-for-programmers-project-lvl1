a28e8cde6c17a39b9838b1f8ff3d3ae8cd3e1a84
Ihor K <lov3catch@gmail.com>
2021-07-27 21:50:36 +0300

    - add

--- app/config/database.js
+++ app/config/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: 'bookshelf',
       settings: {
         client: 'postgres',
-          host: env('DATABASE_HOST', 'localhost'),
+          host: env('DATABASE_HOST', 'db'),
           port: env.int('DATABASE_PORT', 5432),
-          database: env('DATABASE_NAME', 'strapi'),
-          username: env('DATABASE_USERNAME', 'strapi'),
-          password: env('DATABASE_PASSWORD', 'strapi'),
+          database: env('DATABASE_NAME', 'postgres'),
+          username: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', 'example'),
           ssl: env('DATABASE_SSL', false)
       },
       options: {
