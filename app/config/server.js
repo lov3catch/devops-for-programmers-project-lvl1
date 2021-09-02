2ec99f72e56ccf9e52521c28b67c582aa57104ef
Ihor K <lov3catch@gmail.com>
2021-09-02 11:40:04 +0300

    - add port and host

--- app/config/server.js
+++ app/config/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  port: env.int('PORT', 8080),
+  url: 'http://localhost',
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'b263b572c8ca1d02bb9a656251fff01f'),
